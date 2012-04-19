6595ec55bf9903b88d3dda99af6d87449cea634c
Matt Phillips <mathewrphillips@gmail.com>
2012-04-19 15:44:27 -0500

    finished initial implementation

---
+++ Dictionizer.Tests/ObjectConverterTests.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Dictionizer;
+
+namespace Dictionizer.Tests
+{
+    class Simple
+    {
+        public string Prop1 { get; set; }
+        public long Prop2 { get; set; }
+    }
+
+    class Complex
+    {
+        public Simple Simple1 { get; set; }
+        public string Prop1 { get; set; }
+    }
+
+
+    [TestFixture]
+    public class ObjectConverterTests
+    {
+        [Test]
+        public void SimpleClassConversionIsSuccessful()
+        {
+            var testObj = new Simple
+                              {
+                                  Prop1 = "Hello",
+                                  Prop2 = 100
+                              };
+            var result = testObj.ToDictionary();
+            Assert.IsInstanceOf<IDictionary<string, object>>(result);
+            Assert.AreEqual("Hello",result["Prop1"]);
+            Assert.AreEqual(100,result["Prop2"]);
+        }
+
+        [Test]
+        public void ComplexClassConversionWithoutDeepCopy()
+        {
+            var testObj = new Complex
+                              {
+                                  Simple1 = new Simple
+                                                {
+                                                    Prop1 = "Hello",
+                                                    Prop2 = 100   
+                                                },
+                                  Prop1 = "Hello"
+                              };
+
+            var result = testObj.ToDictionary();
+            Assert.IsInstanceOf<IDictionary<string, object>>(result);
+            Assert.AreEqual("Hello",result["Prop1"]);
+            Assert.IsInstanceOf<Simple>(result["Simple1"]);
+        }
+
+        [Test]
+        public void ComplexClassConversionWithDeepCopy()
+        {
+            var testObj = new Complex
+            {
+                Simple1 = new Simple
+                {
+                    Prop1 = "Hello",
+                    Prop2 = 100
+                },
+                Prop1 = "Hello"
+            };
+
+            var result = testObj.ToDictionary(true);
+            Assert.IsInstanceOf<IDictionary<string, object>>(result);
+            Assert.IsInstanceOf<IDictionary<string,object>>(result["Simple1"]);
+            Assert.AreEqual("Hello", result["Prop1"]);
+            
+        }
+    }
+}
---
+++ Dictionizer/ObjectConverter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dictionizer
+{
+    public static class ObjectConverter
+    {
+        /// <summary>
+        /// Convert the public properties of a class into a dictionary
+        /// </summary>
+        /// <param name="item">The object to be turned into a dictionary</param>
+        /// <returns>IDictionary<string,object></returns>
+        public static IDictionary<string,object> ToDictionary(this object item)
+        {
+            return ToDictionary(item, false);
+        }
+        
+        /// <summary>
+        /// Convert the public properties of a class into a dictionary
+        /// </summary>
+        /// <param name="item">The object to be turned into a dictionary</param>
+        /// <param name="deepCopy">Whether reference type properties should also be converted to dictionaries</param>
+        /// <returns>IDictionary<string,object></returns>
+        public static IDictionary<string,object> ToDictionary(this object item, bool deepCopy)
+        {
+            var result = new Dictionary<string, object>();
+            foreach (var prop in item.GetType().GetProperties())
+            {
+                var name = prop.Name;
+                if (!prop.PropertyType.IsValueType && deepCopy)
+                {       
+                    result[name] = prop.GetValue(item, null).ToDictionary(true);                        
+                }else
+                {
+                    result[name] = prop.GetValue(item, null);
+                }
+            }
+            return result;
+        }
+    }
+}
